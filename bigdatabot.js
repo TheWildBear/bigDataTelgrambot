408f2f8041742642c8ca7492911361f8ef305755
TheWildBear <TheWildBear@users.noreply.github.com>
2017-12-03 13:52:29 +0100

    adding optout and checklogging
    with these two commands /optout and /checklogging the users can check if they are logged and can end the logging for them

--- bigdatabot.js
+++ bigdatabot.js
@@ -6,37 +6,34 @@
 const Telebot = require('telebot');
 const bot = new Telebot({
         token: 'your_token',
-        limit: 0});
+        limit: 1000});
 const util = require('util');
 const mysql = require('mysql');
+const hash = require('hash-int');
 var fs = require('fs');
 var log;
 var db = mysql.createConnection({
         host: "localhost",
         user: "your_user",
-        password: "yourpwd!",
+        password: "your_pwd",
         database: "db"
 });
 
 db.connect(function(err) {
         if (err) throw err;
-        //db.query("SELECT * FROM messagetable", function (err, result, fields) {
-        //      if (err) throw err;
-        //      console.log(result);
-        //});
 });
 
 bot.start();
 
 bot.on('text' ,(msg) => {
-        var checkoptin = "SELECT COUNT(*) AS checkOptin FROM optintable where userid = " + msg.from.id + ";";
+        var checkoptin = "SELECT COUNT(*) AS checkOptin FROM optintable where userid = " + hash(msg.from.id) + ";";
         db.query(checkoptin, function(err, rows){
                 console.log(util.inspect(rows[0].checkOptin,false,null));
                 if(rows[0].checkOptin==1){
                         var sqlcmd = "INSERT INTO messagetable (msgid, userid, groupid, text, chattype) VALUES ?";
-                        var values = [[msg.message_id, msg.from.id, msg.chat.id, msg.text, msg.chat.type]];
+                        var values = [[msg.message_id, hash(msg.from.id), msg.chat.id, msg.text, msg.chat.type]];
                         db.query(sqlcmd, [values], function(err, result){
-                                console.log("debug message");
+                                console.log("message added");
                         });
                 }
         });
@@ -44,8 +41,22 @@ bot.on('text' ,(msg) => {
 
 bot.on('/optin', (msg) => {
         let sqlcmd = "INSERT INTO optintable (userid) VALUES ?";
-        var values = [[msg.from.id]];
-        db.query(sqlcmd, [values], function(err, resule){
-                console.log("user added to whitlist: " + msg.from.id);
+        var values = [[hash(msg.from.id)]];
+        db.query(sqlcmd, [values], function(err, result){
+                msg.reply.text("You opted in for data collection!");
+        });
+});
+
+bot.on('/optout', (msg) =>{
+        let sqlcmd = "DELETE FROM optintable WHERE userid = " + hash(msg.from.id) + ";";
+        db.query(sqlcmd, function(err, result){
+                msg.reply.text("You opted out for data collection");
+        });
+});
+
+bot.on('/checklogging', (msg) => {
+        let sqlcmd = "SELECT COUNT(*) AS logging FROM optintable where userid = " + hash(msg.from.id) + ";";
+        db.query(sqlcmd, function(err, rows){
+                msg.reply.text("Your current status is: " + util.inspect(rows[0].logging,false,null));
         });
 });
