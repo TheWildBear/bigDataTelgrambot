80e0771d907f2dc88de847799882984bd1f29d75
TheWildBear <TheWildBear@users.noreply.github.com>
2018-01-12 22:24:38 +0100

    change db access to pooling

--- bigdatabot.js
+++ bigdatabot.js
@@ -5,8 +5,8 @@
 /*jshint esversion: 6 */
 
 /*
-Version 1.0.0
-*/
+ *Version 1.0.1
+ */
 
 const Telebot = require('telebot');
 const bot = new Telebot({
@@ -15,9 +15,11 @@ const bot = new Telebot({
 const util = require('util');
 const mysql = require('mysql'); 
 const hash = require('hash-int');
-var fs = require('fs');
+const fs = require('fs');
+var admin = 8846643;
 var log;
-var db = mysql.createConnection({
+var db = mysql.createPool({
+	connectionLimit : 100,
 	host: "localhost",
 	user: "your_user",
 	password: "your_pwd",
@@ -25,114 +27,191 @@ var db = mysql.createConnection({
 	charset : 'utf8mb4'
 });
 
-db.connect(function(err) {
-	if (err) throw err;
+var dbwrite = mysql.createPool({
+	connectionLimit : 100,
+        host: "localhost",
+        user: "your_user",
+        password: "your_pwd",
+        database: "db",
+        charset : 'utf8mb4'
 });
 
 bot.start();
 
 bot.on('text' ,(msg) => {
 	var checkoptin = "SELECT COUNT(*) AS checkOptin FROM optintable where userid = " + hash(msg.from.id) + ";";
-	db.query(checkoptin, function(err, rows){
-		if(err) throw err;
-		if(rows[0].checkOptin==1){
-			var sqlcmd = "INSERT INTO messagetable (msgid, userid, groupid, text, chattype) VALUES ?";
-		        var values = [[msg.message_id, hash(msg.from.id), msg.chat.id, msg.text, msg.chat.type]];
-		        db.query(sqlcmd, [values]);
-		}
+	dbwrite.getConnection(function(err, connection){
+		connection.query(checkoptin, function(err, rows){
+			if(err) throw err;
+			if(rows[0].checkOptin==1){
+				var sqlcmd = "INSERT INTO messagetable (msgid, userid, groupid, text, chattype) VALUES ?";
+		        	var values = [[msg.message_id, hash(msg.from.id), msg.chat.id, msg.text, msg.chat.type]];
+		        	connection.query(sqlcmd, [values]);
+			}
+			connection.release();
+		});
 	});
 });
 
 bot.on('/optin', (msg) => {
-	let sqlcmd = "INSERT INTO optintable (userid) VALUES ?";
+	let sqlcmd = "INSERT IGNORE INTO optintable (userid) VALUES ?";
 	var values = [[hash(msg.from.id)]];
-	db.query(sqlcmd, [values], function(err, result){
-		if(err) throw err;
-		bot.deleteMessage(msg.chat.id, msg.message_id);
-		msg.reply.text("You opted in for data collection!");
+	dbwrite.getConnection(function(err, connection){
+		db.query(sqlcmd, [values], function(err, result){
+			if(err) throw err;
+			bot.deleteMessage(msg.chat.id, msg.message_id);
+			msg.reply.text("You opted in for data collection!");
+			connection.release();
+		});
 	});
 });
 
 bot.on('/optout', (msg) =>{
 	let sqlcmd = "DELETE FROM optintable WHERE userid = ?";
 	let values = [[hash(msg.from.id)]];
-	db.query(sqlcmd, [values], function(err, result){
-		if(err) throw err;
-		bot.deleteMessage(msg.chat.id, msg.message_id);
-		msg.reply.text("You opted out for data collection");
+	dbwrite.getConnection(function(err, connection) {
+		connection.query(sqlcmd, [values], function(err, result){
+			if(err) throw err;
+			bot.deleteMessage(msg.chat.id, msg.message_id);
+			msg.reply.text("You opted out for data collection");
+			connection.release();
+		});
 	});
 });
 
 bot.on('/checklogging', (msg) => {
 	let sqlcmd = "SELECT COUNT(*) AS logging FROM optintable where userid = ?";
 	let values = [[hash(msg.from.id)]];
-	db.query(sqlcmd, [values], function(err, rows){
-		if(err) throw err;
-		bot.deleteMessage(msg.chat.id, msg.message_id);
-		msg.reply.text("Your current status is: " + util.inspect(rows[0].logging,false,null));
+	db.getConnection(function(err, connection) {
+		connection.query(sqlcmd, [values], function(err, rows){
+			if(err) throw err;
+			bot.deleteMessage(msg.chat.id, msg.message_id);
+			msg.reply.text("Your current status is: " + util.inspect(rows[0].logging,false,null));
+			connection.release();
+		});
 	});
 });
 
-bot.on('/amount', (msg) => {
+//sends a msg with the total amount of msgs saved by the bot
+bot.on('/total_amount', (msg) => {
         let sqlcmd = "SELECT COUNT(*) AS amount FROM messagetable";
-        db.query(sqlcmd, function(err, rows){
-		if(err) throw err;
-		bot.deleteMessage(msg.chat.id, msg.message_id);
-                msg.reply.text("The current amount of overall msgs is: " + util.inspect(rows[0].amount,false,null));
-        });
+	db.getConnection(function(err, connection) {
+	        connection.query(sqlcmd, function(err, rows){
+			if(err) throw err;
+			bot.deleteMessage(msg.chat.id, msg.message_id);
+	                msg.reply.text("The current amount of overall msgs is: " + util.inspect(rows[0].amount,false,null));
+			connection.release();
+        	});
+	});
 });
 
-bot.on('/ownamount', (msg) => {
+//sends a msg with the overall amount of msgs sent by the sender of command
+bot.on('/total_ownamount', (msg) => {
 	bot.sendAction(msg.chat.id, 'typing');
         let sqlcmd = "SELECT COUNT(*) AS amount, truncate(avg(char_length( `text` )),2) AS AVG_Length FROM messagetable WHERE userid = " + hash(msg.from.id) + " AND `text` NOT LIKE '/%';";
-        db.query(sqlcmd, function(err, rows){
-		if(err)throw err;
-                msg.reply.text("Your current  amount of your own msgs is: " + util.inspect(rows[0].amount,false,null) + " and the AVG_length is: " + rows[0].AVG_Length, { asReply: true });
-        });
+	db.getConnection(function(err, connection) {
+        	connection.query(sqlcmd, function(err, rows){
+			if(err)throw err;
+                	msg.reply.text("Your current  amount of your own msgs is: " + util.inspect(rows[0].amount,false,null) + " and the AVG_length is: " + rows[0].AVG_Length, { asReply: true });
+			connection.release();
+	        });
+	});
+		
 });
 
+//sends a msg with the amount of msgs sent by the sender of command in this group
+bot.on('/ownamount', (msg) => {
+        bot.sendAction(msg.chat.id, 'typing');
+	let groupid = msg.chat.id
+        let sqlcmd = "SELECT COUNT(*) AS amount, truncate(avg(char_length( `text` )),2) AS AVG_Length FROM messagetable WHERE userid = " + hash(msg.from.id) + " AND `text` NOT LIKE '/%' AND groupid = " + groupid + ";";
+	db.getConnection(function(err, connection) {
+	        connection.query(sqlcmd, function(err, rows){
+        	        if(err)throw err;
+                	msg.reply.text("Your current  amount of your own msgs is: " + util.inspect(rows[0].amount,false,null) + " and the AVG_length is: " + rows[0].AVG_Length, { asReply: true });
+			connection.release();
+	        });
+	});
+});
+
+//deletes all msgs by sender of command :(
 bot.on('/deletemymsgs', (msg) => {
         let sqlcmd = "DELETE FROM messagetable WHERE userid = " + hash(msg.from.id) + ";";
-        db.query(sqlcmd, function(err){
-		if(err) throw err;
-		bot.deleteMessage(msg.chat.id, msg.message_id);
-                msg.reply.text("Your msgs have been deleted :(");
-        });
+	dbwrite.getConnection(function(err, connection) {
+	        connection.query(sqlcmd, function(err){
+			if(err) throw err;
+			bot.deleteMessage(msg.chat.id, msg.message_id);
+	                msg.reply.text("Your msgs have been deleted :(");
+        	});
+	});
 });
 
+//counts the amount of msgs that have the requested chars in them
+bot.on(/^\/count (.+)$/, (msg, props) => {
+        bot.sendAction(msg.chat.id, 'typing');
+        let searchtext = "";
+        let sqlcmd = "SELECT count(text) AS `text` FROM messagetable WHERE text LIKE ?";
+        var values = [["%" + props.match[1] + "%"]];
+	db.getConnection(function(err, connection) {
+	        connection.query(sqlcmd, [values], function(err, rows){
+        	        if (err) throw err;
+                	msg.reply.text("Your selected amount of msgs is: " + rows[0].text, { asReply: true });
+	        });
+		connection.release();
+	});
+});
+
+//sends a list containing the top 10 people writing msgs
+bot.on('/top', (msg) => {
+	bot.sendAction(msg.chat.id, 'typing');
+        let sqlcmd = "SELECT DISTINCT COUNT( `msgid` ) AS `Msgs`, `userid` AS `User`, truncate(avg(char_length( `text` )),2) AS AVG_Length FROM `db`.`messagetable` AS `messagetable` WHERE `text` NOT LIKE '/%' GROUP BY `userid` ORDER BY `Msgs` DESC LIMIT 10;"
+	db.getConnection(function(err, connection) {
+		connection.query(sqlcmd, function(err, rows){
+			if(err) throw err;
+			let result = "The top people writing msgs are: \n";
+			for(var i in rows)
+			{
+				result = result + i + ". Messages: " + rows[i].Msgs + "\t\tUser: " + rows[i].User + "\t\tAVG_Length: " + rows[i].AVG_Length;
+				result = result + "\n";
+			}
+			msg.reply.text(result);
+			connection.release();
+	        });
+	});
+});
+
+//sends a list containing the top 10 people writing msgs in the group send from
+bot.on('/topingroup', (msg) => {
+        bot.sendAction(msg.chat.id, 'typing');
+	let groupid = msg.chat.id
+        let sqlcmd = "SELECT DISTINCT COUNT( `msgid` ) AS `Msgs`, `userid` AS `User`, truncate(avg(char_length( `text` )),2) AS AVG_Length FROM `db`.`messagetable` AS `messagetable` WHERE `text` NOT LIKE '/%' AND groupid = " + groupid + " GROUP BY `userid` ORDER BY `Msgs` DESC LIMIT 10;"
+        db.getConnection(function(err, connection) {
+		connection.query(sqlcmd, function(err, rows){
+                	if(err) throw err;
+	                let result = "In this group the top msg writing people are: \n";
+        	        for(var i in rows)
+                	{
+	                        result = result + i + ". Messages: " + rows[i].Msgs + "\t\tUser: " + rows[i].User + "\t\tAVG_Length: " + rows[i].AVG_Length;
+        	                result = result + "\n";
+	                }
+        	        msg.reply.text(result);
+			connection.release();
+        	});	
+	});
+});
+
+//sends the licence
+bot.on('/licence', (msg) => {
+        msg.reply.text('Welcome!\nThis bot is from @thewildbear.\nHe is awsome!\nCheckout his github\nhttps://github.com/TheWildBear\n \nCopyright 2017 TheWildBear\nThis bot is licensed under the MIT License!\nSpread the love for free Software!', { parseMode: 'markdown' });
+});
+
+
+//sends link to repo and informs about the bot
 bot.on(['/start', '/help'], (msg) => {
 	let startmsg = "Commands:\n/optin (agree to collecting your messages)\n/optout (disable collection)\n/checklogging (check collection status)\n/amount (total amount of messages collected)\n/ownamount (number of your collected messages)\n/deletemymsgs (remove all collected data from the DB)\n\nThis bot collects data which will be used in the future for analysis and learning big data. It's opt in and does not collect any data if you are opted out. I would appreciate if you would donate me you're data!\nP.S. All data is anonymized";
 	bot.deleteMessage(msg.chat.id, msg.message_id);
 	msg.reply.text(startmsg);
 });
 
-
-bot.on(/^\/count (.+)$/, (msg, props) => {
-	bot.sendAction(msg.chat.id, 'typing');
-	let searchtext = "";
-        let sqlcmd = "SELECT count(text) AS `text` FROM messagetable WHERE text LIKE ?";
-	var values = [["%" + props.match[1] + "%"]];
-       	db.query(sqlcmd, [values], function(err, rows){
-		if (err) throw err;
-		msg.reply.text("Your selected amount of msgs is: " + rows[0].text, { asReply: true });
-        });
-});
-
-bot.on('/top', (msg) => {
-	bot.sendAction(msg.chat.id, 'typing');
-        let sqlcmd = "SELECT DISTINCT COUNT( `msgid` ) AS `Msgs`, `userid` AS `User`, truncate(avg(char_length( `text` )),2) AS AVG_Length FROM `db`.`messagetable` AS `messagetable` WHERE `text` NOT LIKE '/%' GROUP BY `userid` ORDER BY `Msgs` DESC LIMIT 10;"
-	db.query(sqlcmd, function(err, rows){
-		if(err) throw err;
-		let result = "";
-		for(var i in rows)
-		{
-			result = result + i + ". Messages: " + rows[i].Msgs + "\t\tUser: " + rows[i].User + "\t\tAVG_Length: " + rows[i].AVG_Length;
-			result = result + "\n";
-		}
-		msg.reply.text(result);
-        });
-});
-
-bot.on('/licence', (msg) => {
-        msg.reply.text('Welcome!\nThis bot is from @thewildbear.\nHe is awsome!\nCheckout his github\nhttps://github.com/TheWildBear\n \nCopyright 2017 TheWildBear\nThis bot is licensed under the MIT License!\nSpread the love for free Software!', { parseMode: 'markdown' });
+bot.on('/ping', (msg) => {
+	msg.reply.text("Pong, Pung, Ping! Ente!!!! FOOOOOOOSSS!!!");
 });
